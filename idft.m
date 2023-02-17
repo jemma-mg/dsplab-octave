2651ac09087356d0554eeb2f41db5b7c79e0a2c4
Jemma Mariya George <83303483+jemma-mg@users.noreply.github.com>
2023-02-17 23:49:23 +0530

    IDFT

---
+++ idft.m
@@ -0,0 +1,25 @@
+clc;clf;clear all;close all;
+
+x = input("Enter the sequence: ");
+N = length(x);
+n_t = 0:length(x)-1;
+subplot(211);
+stem(n_t,x);
+title("Fourier Transform of signal");
+xlabel("frequency");
+ylabel("amplitude");
+
+X=[];
+for k=1:N
+  a=0;
+  for n=1:N
+    a = a + x(n).*exp(j*2*pi*(k-1)*(n-1)/N);
+  endfor
+  X = [X a];
+endfor
+X = X/N;
+subplot(212);
+stem(n_t,X);
+title("Original signal");
+xlabel("time");
+ylabel("amplitude");
